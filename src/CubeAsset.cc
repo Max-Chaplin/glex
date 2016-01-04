c79d8f61fa75a2b99ea5eb6e22519c07ab2d7e21
Maximillian Chaplin (student) <mc537@moco207L-b5.university.brighton.ac.uk>
2016-01-04 11:52:16 +0000

    Managed to draw a cube on the screen using the vertex buffer and element buffer.

--- src/CubeAsset.cc
+++ src/CubeAsset.cc
@@ -3,16 +3,36 @@
 CubeAsset::CubeAsset() {
   // model coordinates, origin at centre.
   GLfloat vertex_buffer [] {
-    -0.5, -0.5, -0.5
-    , -0.5,  0.5, -0.5
-    ,  0.5, -0.5, -0.5
-    ,  0.5,  0.5, -0.5
+    -0.5, -0.5, -0.5    //0
+    , -0.5,  0.5, -0.5  //1
+    ,  0.5, -0.5, -0.5  //2
+    ,  0.5,  0.5, -0.5  //3
+      
+    , -0.5, -0.5,  0.5  //4
+    , -0.5,  0.5,  0.5  //5
+    ,  0.5, -0.5,  0.5  //6
+    ,  0.5,  0.5,  0.5  //7
   };
 
-  element_buffer_length = 6;
+  element_buffer_length = 36;
   GLuint element_buffer []  {
     0, 1, 2
-    , 1, 3, 2
+      , 1, 3, 2//front
+      
+      , 1, 5, 7
+      , 1, 7, 3//top
+
+      , 3, 6, 2
+      , 3, 7, 6//right
+
+      , 7, 4, 6
+      , 7, 5, 4//back
+                  
+      , 5, 1, 0
+      , 5, 4, 0//left
+
+      , 6, 4, 0
+      , 6, 2, 0//bottom
   };
 
   // Transfer buffers to the GPU
@@ -23,7 +43,7 @@ CubeAsset::CubeAsset() {
 
   // immediately bind the buffer and transfer the data
   glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_token);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 12, vertex_buffer, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 24, vertex_buffer, GL_STATIC_DRAW);
 
   glGenBuffers(1, &element_buffer_token);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_buffer_token);
