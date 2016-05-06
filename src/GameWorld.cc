fbfd0fea201ec27ce0091e4fa3cd90db8cd6b20b
Maximillian Chaplin (student) <mc537@mowa202L-c2.university.brighton.ac.uk>
2016-05-06 14:07:43 +0100

    Implemented an array to draw the cubes. This is a much easier way of doing this as before I had to put in each cube one at a time and set each of their co-ordinates.

--- src/GameWorld.cc
+++ src/GameWorld.cc
@@ -2,7 +2,33 @@
 
 GameWorld::GameWorld (ApplicationMode mode) {
   asset_manager = std::make_shared<GameAssetManager>(mode);
-  asset_manager->AddAsset(std::make_shared<CubeAsset>(1.0f,1.0f, 1.0f));
+  //asset_manager->AddAsset(std::make_shared<CubeAsset>(1.0f,1.0f, 1.0f));
+  int X, Y;
+  int Z = 1;
+  int Xaxis = 11;
+  int Yaxis = 11;
+
+    int space[Xaxis][Yaxis] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+  };
+
+  for(X = 0; X < Xaxis; X++){
+    for(Y = 0; Y < Yaxis; Y++){
+      if(space[Y][X] == 1){
+	asset_manager->AddAsset(std::make_shared<CubeAsset>((X), -1.0f, (Y*Z)));
+      }
+    }
+  }
 }
 
 
